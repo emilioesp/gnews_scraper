7d4cbfa536a743892f6ccccc1c36093ccf31fc26
Mario Moctezuma <m.a.mocte@gmail.com>
2021-08-10 12:12:09 -0500

    update imageas, create SMA_7d_noticias_sep and time_series_facet script

--- time_series_facet.py
+++ time_series_facet.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import numpy as np
 import os
 
@@ -64,15 +63,15 @@ paises_codes = {'ar':'ARGENTINA',
                 've' : 'VENEZUELA'
                }
 paises_codes_correct = {'ar':'ARGENTINA',
-                'co':'COLOMBIA',
-                'mx':'MÉXICO',
-                'cl':'CHILE',
-                'ec':'ECUADOR',
-                'pe':'PERÚ',
-                'gt':'GUATEMALA',
-                'sv':'EL SALVADOR',
-                'hn':'HONDURAS',
-                'br':'BRASIL',
+                'co': 'COLOMBIA',
+                'mx': 'MÉXICO',
+                'cl': 'CHILE',
+                'ec': 'ECUADOR',
+                'pe': 'PERÚ',
+                'gt': 'GUATEMALA',
+                'sv': 'EL SALVADOR',
+                'hn': 'HONDURAS',
+                'br': 'BRASIL',
                 'uy': 'URUGUAY',
                 'pr': 'PUERTO RICO',
                 'bz': 'BELICE',
@@ -80,21 +79,21 @@ paises_codes_correct = {'ar':'ARGENTINA',
                 'tt': 'TRINIDAD Y TOBAGO',
                 'jm': 'JAMAICA',
                 'sr': 'SURINAME',
-                'gf' : 'GUYANA FRANCESA',
+                'gf': 'GUYANA FRANCESA',
                 'do': 'REPÚBLICA DOMINICANA',
-                'ni' : 'NICARAGUA',
-                'py' : 'PARAGUAY',
-                'pa' : 'PANAMÁ',
+                'ni': 'NICARAGUA',
+                'py': 'PARAGUAY',
+                'pa': 'PANAMÁ',
                 'bo': 'BOLIVIA',
-                'cr' : 'COSTA RICA',
-                've' : 'VENEZUELA'
+                'cr': 'COSTA RICA',
+                've': 'VENEZUELA'
                }
 
-paises = ['ar', 'bb', 'bo', 'br', 'bz', 'cl', 'co', 'cr', 'do', 'ec', 'gf', 'gt', 'hn', 'jm', 'mx', 'ni', 'pa', 'pe', 'pr', 'py', 'sr', 'sv', 'tt', 'uy', 've']
-#serie_vol = go.Figure()
+
+paises = ['ar', 'bb', 'bo', 'br', 'bz', 'cl', 'co', 'cr', 'do', 'ec', 'gt', 'hn', 'jm', 'mx', 'ni', 'pa', 'pe', 'pr', 'py', 'sr', 'sv', 'tt', 'uy', 've']
+# Se quitó gf
+df = pd.DataFrame()
 for pais in paises:
-    serie_vol = go.Figure()
-    print(pais)
     df_news = pd.read_csv(f'news_cln/news_{pais}_cln.csv', sep='\t')
     df_news['date'] =  pd.to_datetime(df_news['date'], format='%Y-%m-%d')
     df_news.sort_values('date', inplace = True)
@@ -108,45 +107,41 @@ for pais in paises:
     df_date = s.to_frame().reset_index().rename({'index': 'date', 'link': 'count'},axis=1)
     df_date = df_date[df_date['date']>= '2017-01-01']
     # SMA 7-days
-    df_date['SMA_7'] = df_date['count'].rolling(7, min_periods=1).mean()*10
-    #CMA
-    df_date['CMA'] = df_date['count'].expanding().mean()
-    # EMA 7 days
-    df_date['EMA_7'] = df_date['count'].ewm(span = 7, adjust=False).mean()
-    X = df_date['date'].tolist()
-    Y = df_date['SMA_7'].tolist()
-    serie_vol.add_trace(go.Scatter(x=X, y=Y,
-                            mode='lines',
-                            name=paises_codes[pais].title(),
-                            marker=dict(color=PAISES_BID[paises_codes[pais]])
-                            #visible=True if prov in main else 'legendonly'
-                           ))
-    serie_vol.update_layout( #showlegend=False,
-                           paper_bgcolor='rgba(0,0,0,0)',
-                           plot_bgcolor='rgba(0,0,0,0)',
-                           yaxis_title="# de noticias",
-                           margin = dict(l=1, r=1, b=5, t=7),
-                           font = dict(color='black'))
-    # serie_vol.update_xaxes(
-    #     rangeslider_visible=True,
-    #     rangeselector=dict(
-    #         buttons=list([
-    #             dict(count=1, label="1m", step="month", stepmode="backward"),
-    #             dict(count=6, label="6m", step="month", stepmode="backward"),
-    #             dict(count=1, label="YTD", step="year", stepmode="todate"),
-    #             dict(count=1, label="1y", step="year", stepmode="backward"),
-    #             dict(step="all")
-    #         ]), bgcolor='#272B30'
-    #     )
-    # )
-    serie_vol.update_layout(
-        title={
-            # 'text': f"Volumen histórico de noticias migratorias en {paises_codes_correct[pais].title()} | Media Móvil de 7 días",
-            'text': f"{paises_codes_correct[pais].title()}",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
+    df_date['Núm. noticias'] = df_date['count'].rolling(7, min_periods=1).mean()*10
+    df_date['país'] = [paises_codes_correct[pais]] * len(df_date)
+    df = pd.concat([df, df_date])
 
-    #serie_vol.write_html(f"images/SMA_7d_noticias_{pais}.html")
-    serie_vol.write_image(f"images/SMA_7d_noticias_{pais}.png")
+fig = px.line(df, x='date', y = 'Núm. noticias',
+              color_discrete_sequence=[k[1] for k in PAISES_BID.items()],
+              color='país',
+              facet_row_spacing=0.04,
+              height=900,    
+              facet_col='país', facet_col_wrap=3)
+fig.update_yaxes(matches=None)
+fig.for_each_annotation(lambda a: a.update(text=a.text.replace("país=", "")))
+for axis in fig.layout:
+    if type(fig.layout[axis]) == go.layout.YAxis:
+        fig.layout[axis].title.text = ''
+
+for axis in fig.layout:
+    if type(fig.layout[axis]) == go.layout.XAxis:
+        fig.layout[axis].title.text = ''
+
+fig.update_layout(
+    # keep the original annotations and add a list of new annotations:
+    annotations = list(fig.layout.annotations) + 
+                  [go.layout.Annotation(x=-0.07,
+                          y=0.5,
+                          font=dict(size=14),
+                          showarrow=False,
+                          text="Núm. de noticias",
+                          textangle=-90,
+                          xref="paper",
+                          yref="paper"
+                          )],
+    showlegend=False,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',)
+
+
+fig.write_image(f"images/SMA_7d_noticias_sep.png")
